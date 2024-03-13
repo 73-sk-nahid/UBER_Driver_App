f19423877a430a0198280a248f332ebb86156708
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-03-14 02:17:28 +0600

    uber_driver_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:uberdriverapp/authentication/login_screen.dart';
 
 Future main() async 
 {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //await Firebase.initializeApp();
 
-  await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
+  /*await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
   {
     if(valueOfPermission)
     {
       Permission.locationWhenInUse.request();
     }
-  });
+  });*/
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
