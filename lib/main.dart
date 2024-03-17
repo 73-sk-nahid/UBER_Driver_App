09d1651f669a65fa1169152d124841e051e8af06
Sk. Nahid <nahidsheikh2001@gmail.com>
2024-03-18 00:31:42 +0600

    singup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:uberdriverapp/authentication/login_screen.dart';
 
 Future main() async 
 {
   WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
   /*await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
   {
